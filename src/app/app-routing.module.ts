9e5d3c9d6ec84d7f2b4879b89a80f806981307fa
TahaSekk <sohailtaha128@gmail.com>
2022-08-29 00:51:14 +0500

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'MainModule', loadChildren: () => import('./main-module/main-module.module').then(m => m.MainModuleModule) }, { path: 'UserModule', loadChildren: () => import('./user-module/user-module.module').then(m => m.UserModuleModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
