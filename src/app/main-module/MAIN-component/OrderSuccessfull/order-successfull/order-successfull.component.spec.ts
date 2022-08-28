0e9fc349610dcc74928b9f8c5f7a42e7e34b94f2
TahaSekk <sohailtaha128@gmail.com>
2022-08-29 01:17:52 +0500

    commit

---
+++ src/app/main-module/MAIN-component/OrderSuccessfull/order-successfull/order-successfull.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OrderSuccessfullComponent } from './order-successfull.component';
+
+describe('OrderSuccessfullComponent', () => {
+  let component: OrderSuccessfullComponent;
+  let fixture: ComponentFixture<OrderSuccessfullComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ OrderSuccessfullComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(OrderSuccessfullComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/main-module/main-module.module.ts
+++ src/app/main-module/main-module.module.ts
@@ -3,11 +3,31 @@ import { CommonModule } from '@angular/common';
 
 import { MainModuleRoutingModule } from './main-module-routing.module';
 import { MainModuleComponent } from './main-module.component';
+import { HomeComponent } from './MAIN-component/home/home/home.component';
+import { HeaderComponent } from './MAIN-component/header/header/header.component';
+import { FooterComponent } from './MAIN-component/footer/footer/footer.component';
+import { ContactUsComponent } from './MAIN-component/ContactUs/contact-us/contact-us.component';
+import { ProductsComponent } from './MAIN-component/Products/products/products.component';
+import { ViewProductsComponent } from './MAIN-component/ViewProducts/view-products/view-products.component';
+import { CheckOutComponent } from './MAIN-component/CheckOut/check-out/check-out.component';
+import { OrderSuccessfullComponent } from './MAIN-component/OrderSuccessfull/order-successfull/order-successfull.component';
+import { OrderFailComponent } from './MAIN-component/OrderFail/order-fail/order-fail.component';
+import { SpecialsComponent } from './MAIN-component/Specials/specials/specials.component';
 
 
 @NgModule({
   declarations: [
-    MainModuleComponent
+    MainModuleComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactUsComponent,
+    ProductsComponent,
+    ViewProductsComponent,
+    CheckOutComponent,
+    OrderSuccessfullComponent,
+    OrderFailComponent,
+    SpecialsComponent
   ],
   imports: [
     CommonModule,
