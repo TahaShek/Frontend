a36f59002f3a945b5ec5d44aa7e94ae90883a4d3
TahaSekk <sohailtaha128@gmail.com>
2022-08-29 00:52:51 +0500

    commmit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'MainModule', loadChildren: () => import('./main-module/main-module.module').then(m => m.MainModuleModule) }, { path: 'UserModule', loadChildren: () => import('./user-module/user-module.module').then(m => m.UserModuleModule) }, { path: 'AdminModule', loadChildren: () => import('./admin-module/admin-module.module').then(m => m.AdminModuleModule) }];
+const routes: Routes = [{ path: 'MainModule', loadChildren: () => import('./main-module/main-module.module').then(m => m.MainModuleModule) }, { path: 'UserModule', loadChildren: () => import('./user-module/user-module.module').then(m => m.UserModuleModule) }, { path: 'AdminModule', loadChildren: () => import('./admin-module/admin-module.module').then(m => m.AdminModuleModule) }, { path: 'ManagmentModule', loadChildren: () => import('./managment-module/managment-module.module').then(m => m.ManagmentModuleModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
